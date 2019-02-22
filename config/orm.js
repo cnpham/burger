24856b0cb046425896fae5e99f6c22f2c580e816
cnpham <44155537+cnpham@users.noreply.github.com>
2019-02-22 03:23:14 -0500

    Add files via upload

---
+++ config/orm.js
@@ -0,0 +1,12 @@
+// connect connection.js
+var connection = require("../config/connection.js");
+
+// not exactly sure how to handle the functions
+function selectAll();
+
+function insertOne();
+
+function updateOne();
+
+// export the orm object
+module.exports = orm;
