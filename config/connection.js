4d35fb17cd752691bd4bb8c697bdf8816f13b5f8
cnpham <44155537+cnpham@users.noreply.github.com>
2019-02-22 19:01:00 -0500

    Add files via upload

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,21 @@
 // require mySQL
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
+var connection;
+
+// setup the connection to the JawsDB database for deployment
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  conneciton = msyql.createConnection({
   host: "localhost",
   port: 8080,
   user: "root",
   password: "",
   database: "burgers_db"
 });
+};
+
+connection.connect();
+// export connection
+module.exports = connection;
